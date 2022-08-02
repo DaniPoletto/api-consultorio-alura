0165ea2417a1e6f00f3e34fb75cd9e1b4a4d0c54
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-02 15:10:36 -0300

    Delete doctor | Excluir médico

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -96,4 +96,17 @@ class MedicosController extends AbstractController
 
         return new JsonResponse($medicoExistente);
     }
+
+    /**
+     * @Route("/medicos/{id}", methods={"DELETE"})
+     */
+    public function remove(int $id) : Response
+    {
+        $medico = $this->buscaMedico($id);
+        $this->entityManager->remove($medico);
+        $this->entityManager->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+
 }
