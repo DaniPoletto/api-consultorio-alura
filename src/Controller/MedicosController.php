02851f1e8bd249c0570bb1e203d729cf5c85f546
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-02 14:06:39 -0300

    get doctors | buscar médicos

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -7,8 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MedicosController
+class MedicosController extends AbstractController
 {
     /**
      * @var EntityManagerInterface
@@ -34,6 +35,32 @@ class MedicosController
 
         $this->entityManager->persist($medico);
         $this->entityManager->flush();
+
+        return new JsonResponse($medico);
+    }
+
+    /**
+     * @Route("/medicos", methods={"GET"})
+     */
+    public function buscarTodos() : Response
+    {
+        $repositorioDeMedicos = $this->getDoctrine()->getRepository(Medico::class);
+
+        $medicoList = $repositorioDeMedicos->findAll();
+
+        return new JsonResponse($medicoList);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     */
+    public function buscarUm(Request $request) : Response
+    {
+        $id = $request->get('id');
+
+        $repositorioDeMedicos = $this->getDoctrine()->getRepository(Medico::class);
+
+        $medico = $repositorioDeMedicos->find($id);
 
         return new JsonResponse($medico);
     }
